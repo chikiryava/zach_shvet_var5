f39ff5c8cc416b683570b2a4931d9ffbaa407704
chikiryava <98350681+chikiryava@users.noreply.github.com>
2023-05-25 12:24:37 +0500

    Add files via upload

---
+++ zach_var5_shvets/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace zach_var5_shvets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Заполнить лист";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(42, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(233, 186);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(368, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(291, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите первую букву слова, которое хотите удалить";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(377, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Удалить из списка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(368, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите слово которое хотите добавить";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(377, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 36);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Добавить слово в лист";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(368, 302);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 51);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Отсортировать по алфавиту";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ zach_var5_shvets/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace zach_var5_shvets
+{
+    public partial class Form1 :Form
+    {
+        List<string> mebel = new List<string>( );
+        public Form1 ()
+        {
+            InitializeComponent( );
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            if ( CheckFile( ) )
+            {
+                ReadFromFile( );
+                FillListBox( );                
+            }
+        }
+        public void ReadFromFile ()
+        {
+            try
+            {
+                StreamReader sr = File.OpenText("mebel.txt");
+
+                string [ ] array = sr.ReadLine( ).Split(' ');
+
+                for ( int i = 0; i < array.Length; i++ )
+                {
+                    mebel.Add(array [ i ]);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Файл заполнен неправильно");
+            }
+        }
+        public bool CheckFile ()
+        {
+            if ( !File.Exists("mebel.txt") )
+            {
+                MessageBox.Show("Такого файла не существует");
+                return false;
+            }
+            StreamReader sr = File.OpenText("mebel.txt");
+
+            string str = sr.ReadToEnd( );
+
+            if ( str.Length == 0 )
+            {
+                MessageBox.Show("Файл пуст");
+                return false;
+            }
+            sr.Close( );
+
+            return true;
+        }
+        public void FillListBox ()
+        {
+            button1.Enabled = false;
+
+            listBox1.Items.Clear( );
+
+            for(int i = 0; i<mebel.Count;i++ )
+            {
+                string str = mebel [i];
+                listBox1.Items.Add($"{str}: {str [ 0 ]}");
+            }
+        }
+        public bool CheckTextBox ()
+        {
+            if ( textBox1.Text.Length != 1 )
+            {
+                MessageBox.Show("Вы должны ввести только одну букву");
+                return false;
+            }
+            char symbol = textBox1.Text [ 0 ];
+
+            if ( !char.IsLetter(symbol) )
+            {
+                MessageBox.Show("Вы должны ввести букву");
+                return false;
+            }
+            return true;
+        }
+        public void Delete ()
+        {
+            bool word_found = false;
+
+            for (int i = 0; i<mebel.Count;i++ )
+            {
+                if (mebel[i].ToUpper( ).StartsWith(textBox1.Text.ToUpper( )) )
+                {
+                    mebel.RemoveAt(i);
+                    word_found = true;
+                    break;
+                }
+            }
+            if ( word_found == true )
+            {
+                FillListBox( );
+                MessageBox.Show("Слово было удалено");
+            }
+            else
+            {
+                MessageBox.Show($"Слово, которое начинается на букву {textBox1.Text} не было найдено");
+            }
+        }
+
+        private void button2_Click (object sender, EventArgs e)
+        {
+            if ( mebel.Count != 0 )
+            {
+                if ( CheckTextBox( ) )
+                {
+                    Delete( );
+                }
+            }
+            else
+                MessageBox.Show("Вы сначала должны заполнить лист");
+        }
+
+        private void button3_Click (object sender, EventArgs e)
+            {
+            if ( mebel.Count != 0 )
+            {
+                if ( CheckWord( ) )
+                {
+                    AddWord( );
+                    FillListBox( );
+                }
+            }
+            else
+            {
+                MessageBox.Show("Сначала вы должны заполнить лист");
+            }
+        }
+        public bool CheckWord ()
+        {
+            if ( textBox2.Text.Length == 0 )
+            {
+                MessageBox.Show("Вы не ввели слово");
+                return false;
+            }
+            char symbol = textBox2.Text [ 0 ];
+            if ( !char.IsLetter(symbol) )
+            {
+                MessageBox.Show("Слово должно начинаться с буквы");
+                return false;
+            }
+            return true;
+
+        }
+
+        private void button4_Click (object sender, EventArgs e)
+        {
+            if ( mebel.Count != 0 )
+            {
+                var ordered = from meb in mebel
+                              orderby meb
+                              select meb;
+
+                listBox1.Items.Clear( );
+
+                foreach(var mebel in ordered )
+                {
+                    char first_letter = mebel [ 0 ];
+                    listBox1.Items.Add($"{mebel}: {first_letter}");
+                }
+                MessageBox.Show("Слова были отсортированы по алфавиту");
+            }
+        }
+        public void AddWord ()
+        {
+            bool word_exist = false;
+
+            foreach(string str in mebel )
+            {
+                if(str.ToLower() == textBox2.Text.ToLower( ) )
+                {
+                    word_exist = true;
+                    break;
+                }
+            }
+            if(word_exist == true )
+            {
+                MessageBox.Show("Такое слово уже существует");
+            }
+            else
+            {
+                mebel.Add(textBox2.Text.ToLower( ));
+                MessageBox.Show("Слово было добавлено");
+            }                   
+            
+        }
+            
+    }
+}
